27209e1eb0e4fb8cfce261000119cb168738e5a9
Jarrku <simonvdbroeck@gmail.com>
2018-02-27 00:41:14 +0100

    Drinks overview page

--- src/pages/Drinks/index.js
+++ src/pages/Drinks/index.js
@@ -1,3 +1,145 @@
-const Drinks = () => "Drinks";
+import React, { Fragment } from "react";
+import { Row } from "antd";
 
-export default Drinks;
+import Total from "./Total";
+import Drink from "./Drink";
+
+const initialState = {
+  pils: {
+    label: "Pils/Hoegaarden",
+    price: 2,
+    amount: 0
+  },
+  frisdrank: {
+    label: "Frisdrank",
+    price: 2,
+    amount: 0
+  },
+  koffie: {
+    label: "Koffie/Thee",
+    price: 2,
+    amount: 0
+  },
+  zwaar: {
+    label: "Zware bieren",
+    price: 3,
+    amount: 0
+  },
+  oudercomite: {
+    label: "Oudercomité",
+    price: 2,
+    amount: 0
+  },
+  verwenkoffie: {
+    label: "Verwenkoffie",
+    price: 3,
+    amount: 0
+  },
+  palm: {
+    label: "Palm",
+    price: 2.2,
+    amount: 0
+  },
+  wijnGlas: {
+    label: "Glas Wijn",
+    price: 3,
+    amount: 0
+  },
+  coupe: {
+    label: "Coupe DB/Caramel",
+    price: 3,
+    amount: 0
+  },
+  aperitief: {
+    label: "Porto/Sherry",
+    price: 2.8,
+    amount: 0
+  },
+  wijnFles: {
+    label: "Fles Wijn",
+    price: 16,
+    amount: 0
+  },
+  gebak: {
+    label: "Gebak",
+    price: 1.8,
+    amount: 0
+  }
+};
+
+const stateKeys = Object.keys(initialState);
+
+export default class Drinks extends React.Component {
+  state = {
+    value: 0,
+    ...initialState
+  };
+
+  onChange = ({ currentTarget }) => {
+    const { value, name } = currentTarget;
+    console.log(currentTarget.value);
+
+    this.setState(prevState => ({
+      [name]: {
+        ...prevState[name],
+        amount: parseInt(value, 10)
+      }
+    }));
+  };
+
+  resetState = () => {
+    this.setState({ ...initialState });
+  };
+
+  calculatePrice = () => {
+    return stateKeys.reduce(
+      (total, key) => total + this.state[key].amount * this.state[key].price,
+      0
+    );
+  };
+
+  onIncrement = name => {
+    this.setState(prevState => ({
+      [name]: {
+        ...prevState[name],
+        amount: prevState[name].amount + 1
+      }
+    }));
+  };
+
+  onDecrement = name => {
+    this.setState(prevState => {
+      const amount =
+        prevState[name].amount > 0 ? prevState[name].amount - 1 : 0;
+
+      return {
+        [name]: {
+          ...prevState[name],
+          amount
+        }
+      };
+    });
+  };
+
+  render() {
+    const total = this.calculatePrice();
+    return (
+      <Fragment>
+        <Total total={total} resetState={this.resetState} />
+        <Row type="flex" justify="space-between">
+          {stateKeys.map(key => (
+            <Drink
+              key={key}
+              stateKey={key}
+              amount={this.state[key].amount}
+              label={this.state[key].label}
+              onChange={this.onChange}
+              onDecrement={() => this.onDecrement(key)}
+              onIncrement={() => this.onIncrement(key)}
+            />
+          ))}
+        </Row>
+      </Fragment>
+    );
+  }
+}
